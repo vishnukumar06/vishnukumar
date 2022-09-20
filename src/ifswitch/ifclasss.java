a1663fbfe2356d021b0b7d97f597cd49786d9c2e
vishnukumar06 <seranmadeviv@gmail.com>
2022-09-20 10:48:14 -0700

    vk

---
+++ src/ifswitch/ifclasss.java
@@ -0,0 +1,15 @@
+package ifswitch;
+
+public class ifclasss {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int x=10,y=5;
+if(x>y)
+{
+	System.out.println("x is greater");
+	System.out.println("x value is" +x);
+}
+	}
+
+}
---
+++ src/ifswitch/ifelse.java
@@ -0,0 +1,18 @@
+package ifswitch;
+
+public class ifelse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+  int x=5,y=6,a=4,b=3;
+  if ((x>a)&&(a>y))
+  {
+	  System.out.println("true");
+	    }
+  else 
+  {
+	  System.out.println("false");
+  }
+	}
+
+}
